3143c9b38fd27205668afe3f4d77fc4b6e8d7efc
topyk27 <taufikdwiwahyuputra@gmail.com>
2021-08-12 15:16:22 +0800

    Versi

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,6 +1,10 @@
+<?php 
+	$this->config->load('ptsp_tamu_config',TRUE);
+	$versi = $this->config->item('version','ptsp_tamu_config');
+ ?>
 <footer class="main-footer">
 	<div class="float-right d-none d-sm-block">
-	  <b>Version</b> 1.0.0
+	  <b>Version</b> <?php echo $versi; ?>
 	</div>
 	<strong class="color-change-4x">Copyright &copy; <?php echo date("Y"); ?> <a href="https://topyk27.github.io/"><?php echo $this->session->userdata('cpr'); ?> </a></strong>
 </footer>
