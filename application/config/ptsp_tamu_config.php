3a37371b3569a5f90388267888c7977e47ff5c79
topyk27 <taufikdwiwahyuputra@gmail.com>
2021-08-17 17:56:40 +0800

    Update ptsp_tamu_config.php

--- application/config/ptsp_tamu_config.php
+++ application/config/ptsp_tamu_config.php
@@ -1,6 +1,8 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config['version'] = '2.1.3';
-$config['sipp'] = "sipp"; //nama database di sipp
-$config['ini'] = "ptsp_tamu"; //nama database aplikasi ini
+// $config['sipp'] = "sipp"; //nama database di sipp
+// $config['ini'] = "ptsp_tamu"; //nama database aplikasi ini
+$config['sipp'] = "O3t0pSMpfT"; //nama database di sipp
+$config['ini'] = "O3t0pSMpfT"; //nama database aplikasi ini
  ?>
